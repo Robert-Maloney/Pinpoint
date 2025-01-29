c8d80364577e13e61c48718563a632386d718daa
Robert Maloney <robert.maloney26@mail.dcu.ie>
2025-01-29 12:21:42 +0000

    Added event creation and profile features. Formatted code and app for a cleaner appearance

--- pin_point/map_features/models.py
+++ pin_point/map_features/models.py
@@ -1,17 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import User
-from datetime import datetime
+from django.contrib.auth.models import User, AbstractUser
+from django.conf import settings
 
+from datetime import datetime
 
 # Create your models here.
 
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class User(AbstractUser):
+   email = models.EmailField(unique=True)
+   phone = models.CharField(max_length=15, blank=True, null=True)
+   profile_photo = models.ImageField(
+      upload_to='profile_photos/', 
+      blank=True, 
+      null=True
+   )
+
+   def get_profile_photo(self):
+      if self.profile_photo:
+        return self.profile_photo.url
+      return "/static/icons/default_profile.jpg"  
+   def __str__(self):
+      return self.username
+
+
+
 class Pin(models.Model):
-   title = models.CharField(max_length=255, blank=True, null=True)  # Optional title for the pin
+   title = models.CharField(max_length=255, blank=True, null=True)
    latitude = models.FloatField()
    longitude = models.FloatField()
-   event_time = models.DateTimeField(null=True, blank=True)  # Allow null and blank values
+   event_time = models.DateTimeField(null=True, blank=True)
    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
    def __str__(self):
@@ -24,15 +44,15 @@ class Photo(models.Model):
 
 
 class Event(models.Model):
-   name = models.CharField(max_length=255)
-   description = models.TextField(blank=True, null=True)
-   date_time = models.DateTimeField()
-   location_name = models.CharField(max_length=255)  # e.g., DCU Nubar
-   latitude = models.FloatField()
-   longitude = models.FloatField()
-   private = models.BooleanField(default=False)
-   created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="events")
-   invitees = models.ManyToManyField(User, related_name="event_invitations", blank=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    date_time = models.DateTimeField()
+    location_name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    private = models.BooleanField(default=False)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="events")
+    invitees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="event_invitations", blank=True)
 
-   def __str__(self):
-      return self.name
+    def __str__(self):
+        return self.name
