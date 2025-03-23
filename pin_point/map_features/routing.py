dd7a44bf2aa9750a41e62c5f0dab1e0c9ccddbb6
mbpono24 <michael.beirneponomarev24@mail.dcu.ie>
2025-03-23 13:50:48 +0000

    Test Commit

--- pin_point/map_features/routing.py
+++ pin_point/map_features/routing.py
@@ -6,13 +6,3 @@ from .consumers import ChatConsumer
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<event_id>\w+)/$', ChatConsumer.as_asgi()),
 ]
-
-# application = ProtocolTypeRouter({
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(websocket_urlpatterns)
-#     ),
-# })
-
-# import logging
-# logger = logging.getLogger(__name__)
-# logger.info("WebSocket routing loaded")
