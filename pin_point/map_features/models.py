b2e5aebe51836faf4a7a5af4d0593bcf1e1f73f4
Robert Maloney <robert.maloney26@mail.dcu.ie>
2025-03-30 18:47:44 +0100

    Add friend system and friend related features

--- pin_point/map_features/models.py
+++ pin_point/map_features/models.py
@@ -17,15 +17,23 @@ class User(AbstractUser):
       blank=True, 
       null=True
    )
+   friends = models.ManyToManyField("self", blank=True, symmetrical=True)
 
    def get_profile_photo(self):
       if self.profile_photo:
         return self.profile_photo.url
       return "/static/icons/default_profile.jpg"  
+   
    def __str__(self):
       return self.username
 
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_requests', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='received_requests', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.from_user} → {self.to_user}"
 
 class Pin(models.Model):
    title = models.CharField(max_length=255, blank=True, null=True)
